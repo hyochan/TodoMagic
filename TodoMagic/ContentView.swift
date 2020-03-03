faba6a6069acaf77a8093f5a1d2722d06974a85d
hyochan <dooboolab@gmail.com>
2020-03-03 21:53:27 +0900

    Initial Commit

---
+++ TodoMagic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoMagic
+//
+//  Created by hyochan on 2020/03/03.
+//  Copyright © 2020 dooboolab. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
